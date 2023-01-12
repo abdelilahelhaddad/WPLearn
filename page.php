c48e9b51ff20dff2cfc8f66d846e1c1d4e4047cb
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2023-01-12 16:17:13 +0100

    :fire: :art: Convert HTML Template to WP :sparkles: :globe_with_meridians:

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 get_header();
 
 while (have_posts()) {
-    the_post(); ?>
+  the_post(); ?>
 
 <div class="page-banner">
   <div class="page-banner__bg-image"
@@ -18,20 +18,37 @@ while (have_posts()) {
 
 <div class="container container--narrow page-section">
 
+  <?php
+    $the_parent = wp_get_post_parent_id(get_the_ID());
+    if ($the_parent) : ?>
   <div class="metabox metabox--position-up metabox--with-home-link">
-    <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About
-        Us</a> <span class="metabox__main">Our History</span></p>
+    <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($the_parent); ?>"><i class="fa fa-home"
+          aria-hidden="true"></i> Back to
+        <?php echo get_the_title($the_parent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
   </div>
+  <?php endif; ?>
 
-  <!--
-    <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div>
-    -->
+  <?php
+    $have_children = get_pages(['child_of' => get_the_ID()]);
+    if ($the_parent or $have_children) : ?>
+  <div class="page-links">
+    <h2 class="page-links__title"><a
+        href="<?php echo get_permalink($the_parent); ?>"><?php echo get_the_title($the_parent); ?></a></h2>
+    <ul class="min-list">
+      <?php
+          if ($the_parent) {
+            $the_children_of = $the_parent;
+          } else {
+            $the_children_of = get_the_ID();
+            $args = [
+              'title_li' => NULL,
+              'child_of' => $the_children_of
+            ];
+            wp_list_pages($args);
+          } ?>
+    </ul>
+  </div>
+  <?php endif; ?>
 
   <div class="generic-content">
     <?php the_content(); ?>
