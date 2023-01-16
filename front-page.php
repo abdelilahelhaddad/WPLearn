abf477df80a1c7a2b28388a86e2b67a0bef317b2
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2023-01-16 16:23:16 +0100

    :fire: :art: Convert HTML Template to WP :sparkles: :globe_with_meridians:

--- front-page.php
+++ front-page.php
@@ -21,7 +21,7 @@
       $today = date('Ymd');
       //This custom query orders by events date 
       $homePageEvents = new WP_Query(array(
-        'posts_per_page' => -1,
+        'posts_per_page' => 2,
         'post_type' => 'event',
         'meta_key' => 'event_date',
         'orderby' => 'meta_value_num',
@@ -32,36 +32,14 @@
             'compare' => '>=',
             'value' => $today,
             'type' => 'numeric'
-          ],
-          [
-            'key' => 'related_programs',
-            'compare' => 'LIKE',
-            'value' => '"' . get_the_ID() . '"',
           ]
         ],
       ));
+      // print_r($homePageEvents);
       while ($homePageEvents->have_posts()) {
-        $homePageEvents->the_post(); ?>
-      <div class="event-summary">
-        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-          <span class="event-summary__month"><?php
-                                                $eventDate = new DateTime(get_field('event_date'));
-                                                echo $eventDate->format('M');
-                                                ?></span>
-          <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-        </a>
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a
-              href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-          <p><?php if (has_excerpt()) {
-                  echo get_the_excerpt();
-                } else {
-                  echo wp_trim_words(get_the_content(), 15);
-                } ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn
-              more</a></p>
-        </div>
-      </div>
-      <?php }
+        $homePageEvents->the_post();
+        get_template_part('template-parts/content', 'event');
+      }
       wp_reset_postdata(); ?>
 
       <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>"
