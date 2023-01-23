ad08acf191f484652985cac86de37455473f1994
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2023-01-23 13:34:24 +0100

    :fire: :art: Convert HTML Template to WP :sparkles: :globe_with_meridians:

--- front-page.php
+++ front-page.php
@@ -84,43 +84,36 @@
   </div>
 </div>
 
+<?php
+$args = [
+  'post_type' => 'slideshow',
+];
+
+$slidShowQuery = new WP_Query($args);
+
+?>
+<?php if ($slidShowQuery->have_posts()) : ?>
 <div class="hero-slider">
   <div data-glide-el="track" class="glide__track">
     <div class="glide__slides">
+      <?php while ($slidShowQuery->have_posts()) : $slidShowQuery->the_post(); ?>
       <div class="hero-slider__slide"
-        style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+        style="background-image: url(<?php echo get_field('slideshow_background_image')['url']; ?>);">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
-            <h2 class="headline headline--medium t-center">Free Transportation</h2>
-            <p class="t-center">All students have free unlimited bus fare.</p>
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+            <h2 class="headline headline--medium t-center"><?php the_title(); ?></h2>
+            <p class="t-center"><?php the_field('slideshow_subtitle'); ?></p>
+            <p class="t-center no-margin"><a href="<?php echo get_field('slideshow_button_url'); ?>"
+                class="btn btn--blue"><?php the_field('slideshow_button_text'); ?></a></p>
           </div>
         </div>
       </div>
-      <div class="hero-slider__slide"
-        style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
-        <div class="hero-slider__interior container">
-          <div class="hero-slider__overlay">
-            <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-            <p class="t-center">Our dentistry program recommends eating apples.</p>
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-          </div>
-        </div>
-      </div>
-      <div class="hero-slider__slide"
-        style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
-        <div class="hero-slider__interior container">
-          <div class="hero-slider__overlay">
-            <h2 class="headline headline--medium t-center">Free Food</h2>
-            <p class="t-center">Fictional University offers lunch plans for those in need.</p>
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
-          </div>
-        </div>
-      </div>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?>
     </div>
     <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
   </div>
 </div>
-
+<?php endif; ?>
 
 <?php get_footer(); ?>
